d7546c58de6b7ebccabaaad946fb338fb8b125c7
Jasmine Maquindang <jazzix3@gmail.com>
2023-05-03 20:18:28 -0700

    first commit

---
+++ client/src/pages/Reviews.js
@@ -0,0 +1,24 @@
+import React from "react";
+import TopNav from "../components/Navbar";
+
+const Reviews = () => {
+    
+    return (
+        
+        <>  
+        
+        <TopNav /> 
+
+        <div className="container" id="main-content">
+            <h1>Reviews</h1>
+        
+        </div>
+        </>
+
+
+
+
+    )
+}
+
+export default Reviews;
