620c4fea23df71c308d3490d058ff59744a1362b
Jasmine Maquindang <jazzix3@gmail.com>
2023-05-08 10:59:10 -0700

    fix userprofile image & googlesignin

--- client/src/pages/GoogleSignUp.js
+++ client/src/pages/GoogleSignUp.js
@@ -2,7 +2,7 @@ import React from "react";
 import {Button} from "react-bootstrap";
 import { db } from "../firebase";
 import { getAuth, signInWithPopup, GoogleAuthProvider} from "firebase/auth";
-import { doc, setDoc } from "firebase/firestore";
+import { doc, setDoc, getDoc } from "firebase/firestore";
 import { useNavigate} from "react-router-dom";
 
 const GoogleSignIn = () => {
@@ -12,28 +12,41 @@ const GoogleSignIn = () => {
 
     const handleGoogleSignIn = () => {
         signInWithPopup(auth, provider)
-            .then((result) => {
-                const user = result.user;
-                const userProfile = user.photoURL;
-                const firstName = user.displayName.split(" ")[0];
-                const lastName = user.displayName.split(" ")[1];
-                const email = user.email;
-                return setDoc(doc(db, "users", user.uid), {
-                    userProfile: userProfile,
-                    firstName: firstName,
-                    lastName: lastName,
-                    email: email,
+          .then((result) => {
+            const user = result.user;
+            const userProfile = user.photoURL;
+            const firstName = user.displayName.split(" ")[0];
+            const lastName = user.displayName.split(" ")[1];
+            const email = user.email;
+    
+            const userRef = doc(db, "users", user.uid);
+            return getDoc(userRef).then((doc) => {
+              if (doc.exists()) {
+                return setDoc(userRef, {
+                  userProfile: userProfile,
+                  firstName: firstName,
+                  lastName: lastName,
+                  email: email,
+                }, { merge: true });
+              } else {
+                return setDoc(userRef, {
+                  userProfile: userProfile,
+                  firstName: firstName,
+                  lastName: lastName,
+                  email: email,
                 });
-            })
-            .then(() => {
-                navigate("/Dashboard");
-            })
-            .catch((error) => {
-                console.log(error);
+              }
             });
-    }
-
-    return (
+          })
+          .then(() => {
+            navigate("/Dashboard");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      };
+      
+      return (
         <>
             {/* button that handles the sign in  */}
             <div className="mt-4">
